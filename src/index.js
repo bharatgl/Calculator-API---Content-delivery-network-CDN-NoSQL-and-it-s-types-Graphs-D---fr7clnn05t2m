498f5aa140d9016698d6490e839920698016a89c
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-12-17 17:13:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,61 @@
-const express = require('express')
-const app = express()
-const bodyParser = require("body-parser");
-const port = 3000
-app.use(express.urlencoded());
+const express = require('express');
+const app = express();
 
-// Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.post('/add', (req, res) => {
+  const num1 = req.body.num1;
+  const num2 = req.body.num2;
+  const sum = num1 + num2;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+  if (sum < -1000000 || sum > 1000000) {
+    return res.status(400).json({ status: 'error', message: 'Overflow' });
+  }
 
-app.use(bodyParser.json())
-// your code goes here
+  res.json({ status: 'success', message: 'the sum of given two numbers', sum });
+});
 
+app.post('/sub', (req, res) => {
+  const num1 = req.body.num1;
+  const num2 = req.body.num2;
+  const difference = num1 - num2;
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+  if (difference < -1000000 || difference > 1000000) {
+    return res.status(400).json({ status: 'error', message: 'Overflow' });
+  }
 
-module.exports = app;
+  res.json({ status: 'success', message: 'the difference of given two numbers', difference });
+});
+
+app.post('/multiply', (req, res) => {
+  const num1 = req.body.num1;
+  const num2 = req.body.num2;
+  const result = num1 * num2;
+
+  if (result < -1000000 || result > 1000000) {
+    return res.status(400).json({ status: 'error', message: 'Overflow' });
+  }
+
+  res.json({ status: 'success', message: 'The product of given numbers', result });
+});
+
+app.post('/divide', (req, res) => {
+  const num1 = req.body.num1;
+  const num2 = req.body.num2;
+
+  if (num2 === 0) {
+    return res.status(400).json({ status: 'error', message: 'Cannot divide by zero' });
+  }
+
+  const result = num1 / num2;
+
+  if (result < -1000000 || result > 1000000) {
+    return res.status(400).json({ status: 'error', message: 'Overflow' });
+  }
+
+  res.json({ status: 'success', message: 'The division of given numbers', result });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
